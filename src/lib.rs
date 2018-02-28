b3f2f11759d3f2a592a33b7c561453202e4267bb
Donnie Mattingly <donniewmattingly@gmail.com>
2018-02-28 07:35:27 -0500

    Add ability to generate c library

--- src/lib.rs
+++ src/lib.rs
@@ -20,10 +20,28 @@ use multimap::MultiMap;
 pub mod spectrogram;
 pub mod hash;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use std::os::raw::c_char;
+use std::ffi::{CStr, CString};
+
+#[no_mangle]
+pub extern "C" fn rust_greeting(to: *const c_char) -> *mut c_char {
+    let c_str = unsafe { CStr::from_ptr(to) };
+    let recipient = match c_str.to_str() {
+        Err(_) => "there",
+        Ok(string) => string,
+    };
+
+    CString::new("Hello".to_owned() + recipient)
+        .unwrap()
+        .into_raw()
+}
+
+#[no_mangle]
+pub extern "C" fn rust_greeting_free(s: *mut c_char) {
+    unsafe {
+        if s.is_null() {
+            return;
+        }
+        CString::from_raw(s)
+    };
 }
